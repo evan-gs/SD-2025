b0d72bc49628edc1e28d6917f67ee38667e9283e
Rodinez <mngtandrade@gmail.com>
2025-09-12 20:42:18 -0300

    send and recv das threads

--- processos_multicast.py
+++ processos_multicast.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python3
 
 from scapy.all import *
+from scapy import IP, UDP
 import sys
 import time
 import threading
 
-# Classe personalizada para mensagem e clock lógico
-class mensagem(Packet):
-    name = "Mensagem"
+# Classe de protocolo personalizada para envio de mensagem e clock lógico
+class message(Packet):
+    name = "Message"
     fiels_desc = [
-        ByteEnumField("tipo", 0, {0:"MSG", 1:"ACK"}),
+        ByteEnumField("tipe", 0, {0:"MSG", 1:"ACK"}),
         IntField("timestamp", 0),
         IntField("src_process", 0),
         IntField("dst_process", 0),
         IntField("src_timestamp", 0),
         IntField("dst_timestamp", 0), 
-        StrField("mensagem", "")
+        StrField("text", "")
     ]
 
 # Dicionário dos processos com endereço de loopback e porta
-processos = {
+process = {
     1: ("127.0.0.1", 65001),
     2: ("127.0.0.1", 65002),
     3: ("127.0.0.1", 65003),
     4: ("127.0.0.1", 65004)
 }
 
-num_processos = len(processos)
+num_process = len(process)
 
-id = int(sys.argv[1])
-src, port = processos[id]
+def deal_with_msg():
+    return 0 
 
-clock = 0 
-fila = []
+def tr_receive_msg(id, port, clock, msg_queue):
+    def handle_msg(pkt):
+        clock = deal_with_msg(pkt, id, clock, msg_queue)
+        
+    sniff(filter=f"udp and port {port}", prn=handle_msg)
+
+
+def tr_send_msg(id, clock, msg_queue):
+    while True:
+        text = input("")
+        clock += 1 # pode deixar += id para desincronizar
+        for pid, (host, port) in process.items():
+            pkt = Message(tipe=0, timestamp=clock, src_process=id, dst_process=pid, text=text)
+            send(IP(dst=host)/UDP(dport=port)/pkt)
+        
+        
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    host, port = process[id]
+
+    clock = 0
+    msg_queue = []
+    
+    print(f"Processo: <<P{id}>> rodando em {host}:{port}")
+    
+    th = threading.Thread(target=tr_receive_msg, args=(id, port, clock, msg_queue))
+    th.start()
+    
+    tr_send_msg(id, clock, msg_queue)
